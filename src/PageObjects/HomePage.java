0c7d85a78a1c289c3dc2f8cb30c03a6d57cdcefa
liav <liav916@gmail.com>
2023-11-20 13:23:55 +0200

    added interaction with harpa buttons

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,15 +31,30 @@ public class HomePage extends BasePage {
     By googlesubmitbutton = By.cssSelector("button[type=\"submit\"]");
     By vidiqExtensionButton = By.cssSelector("button[class^=\"vidiq-dropdown-button\"]");
 
+    By harpawait = By.cssSelector("row.DialogSuggestion");
+    By harpawait2 = By.cssSelector("Button.InfoCircleContainer");
 
 
-    public void driverActiveHarpa() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("load-extension=" + BaseTest.getExtensionPathHarpa());
-        }
+
+    public void getVideoSummeryWithHarpa() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.ALT).sendKeys("a").keyUp(Keys.ALT).build().perform();
+        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        By iframeLocator = By.className("__hrp-frame__iframe");
+        WebElement iframeElement = wait.until(ExpectedConditions.presenceOfElementLocated(iframeLocator));
+        driver.switchTo().frame(iframeElement);
+        Thread.sleep(5000);
+        List<WebElement> HarpaButtons = driver.findElements(By.cssSelector("row.DialogSuggestion"));
+        HarpaButtons.get(0).click();
+       Thread.sleep(5000);
+        List<WebElement> HarpaButtons2 = driver.findElements(By.cssSelector(".DialogAskChoice__optionLabel.Button__label"));
+        HarpaButtons2.get(2).click();
+
+    }
     public void BrowseTrhugevideos() throws InterruptedException {
 
-        Thread.sleep(15000);
+        Thread.sleep(5000);
         List<WebElement> elements = driver.findElements(By.className("vidiq-c-hbWfAH"));
         System.out.println("Videos: " + elements);
         for (WebElement element : elements) {
@@ -44,6 +62,8 @@ public class HomePage extends BasePage {
             Set<String> allWindowHandles = driver.getWindowHandles();
             List<String> windowHandlesList = new ArrayList<>(allWindowHandles);
             driver.switchTo().window(windowHandlesList.get(1));
+            getVideoSummeryWithHarpa();
+            Thread.sleep(5000);
             driver.close();
             driver.switchTo().window(windowHandlesList.get(0));
 
